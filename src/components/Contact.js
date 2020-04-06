96795ddc326137258a106a6b87b13e76115891b4
jeanlouis <59745606+jeanlouis6113@users.noreply.github.com>
2020-04-06 15:44:08 +0200

    modifiaction des fichiers

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,25 +6,27 @@ class Contact extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      online: false
+      online: this.props.online
     };
   }
 
   render() {
     return (
-      <div className="Contact">
+      <div className="Contact"
+            onClick={event => {
+                const newOnline = !this.state.online;
+                this.setState({ online: newOnline });
+              }}
+              >
         <img className="avatar" src={this.props.avatar} alt="avatar" />
         <div className="status name">
           <p>{this.props.name}</p>
           <p className="status text">
             <span
               className={this.state.online ? 'status-online' : 'status-offline'}
-              onClick={event => {
-                const newOnline = !this.state.online;
-                this.setState({ online: newOnline });
-              }}
             >
             </span>
+            {this.state.online ? "Online" : "Offline"}
           </p>
         </div>
       </div >
